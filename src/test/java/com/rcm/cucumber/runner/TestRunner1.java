d78bf950ac4d4a5941acc86e39a5c3f07c4638a9
RobertoCalderonM <roberto.a.calderon.m@hotmail.com>
2020-03-13 18:36:50 -0600

    StepNotifications Activated

--- src/test/java/com/rcm/cucumber/runner/TestRunner1.java
+++ src/test/java/com/rcm/cucumber/runner/TestRunner1.java
@@ -13,6 +13,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome=false,
         plugin = {"pretty", "summary","de.monochromata.cucumber.report.PrettyReports:target/cucumber"},
+        stepNotifications = true,
         strict = true,
         snippets = CAMELCASE,
         tags = {"@tagTest1 or @tagTest2"},
--- src/test/java/com/rcm/cucumber/runner/TestRunner2.java
+++ src/test/java/com/rcm/cucumber/runner/TestRunner2.java
@@ -11,6 +11,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome=false,
         plugin = {"pretty", "summary","de.monochromata.cucumber.report.PrettyReports:target/cucumber"},
+        stepNotifications = true,
         strict = true,
         snippets = CAMELCASE,
         tags = {"@tagTest2"},
